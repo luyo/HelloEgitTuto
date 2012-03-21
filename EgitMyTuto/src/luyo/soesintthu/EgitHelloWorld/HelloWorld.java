70d3c45dcb4fa09a7167e0ee354fe204eaadcb5b
luyo <luyo1986@gmail.com>
2012-03-21 21:12:38 +0300

    First commit! Hey!

---
+++ EgitMyTuto/src/luyo/soesintthu/EgitHelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package luyo.soesintthu.EgitHelloWorld;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Egit...");
+	}
+
+}
